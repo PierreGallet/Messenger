202451b0ea2800a182304275eff07975175b2e95
Julien Raspaud <julien.raspaud@student.ecp.fr>
2016-07-22 13:53:06 +0200

    athentification pour l'adresse ngrok

--- index.js
+++ index.js
@@ -8,10 +8,21 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
   res.send('Hello World!');
+});*/
+
+app.get('/', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === "this_is_the_token") {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
 });
 
 app.listen(8888, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 8888!');
 });
