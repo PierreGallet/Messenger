91dba982f3afcf7708b6f11a2077bfe75d3e3e16
Ayaz Badouraly <ayaz.badouraly@via.ecp.fr>
2016-07-25 11:30:26 +0200

    Fixing typo

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ function sendTextMessage(recipientId, messageText) {
 function callSendAPI(messageData) {
   request({
     uri: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: { access_token: config.acces_token },
+    qs: { access_token: config.access_token },
     method: 'POST',
     json: messageData
 
