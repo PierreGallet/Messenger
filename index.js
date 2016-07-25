63b6f262e1c1999c5d584cebf7c7fe868217b228
Julien Raspaud <julien.raspaud@student.ecp.fr>
2016-07-25 15:44:45 +0200

    Ajout de modularités dans le code

--- index.js
+++ index.js
@@ -8,6 +8,8 @@
 var express = require('express');
 var app = express();
 var config = require('./config');
+var mainCtrl = require('./controller/main-controller');
+var buttonCtrl = require('./controller/button-controller')
 
 // Use morgan to print logs
 var morgan  = require('morgan');
@@ -50,13 +52,13 @@ app.post('/', function (req, res) {
         // Iterate over each messaging event
         pageEntry.messaging.forEach(function(messagingEvent) {
         if (messagingEvent.optin) {
-            receivedAuthentication(messagingEvent);
+            mainCtrl.receivedAuthentication(messagingEvent);
         } else if (messagingEvent.message) {
-            receivedMessage(messagingEvent);
+            mainCtrl.receivedMessage(messagingEvent);
         } else if (messagingEvent.delivery) {
-            receivedDeliveryConfirmation(messagingEvent);
+            mainCtrl.receivedDeliveryConfirmation(messagingEvent);
         } else if (messagingEvent.postback) {
-            receivedPostback(messagingEvent);
+            mainCtrl.receivedPostback(messagingEvent);
         } else {
             console.log("Webhook received unknown messagingEvent: ", messagingEvent);
         }
@@ -70,89 +72,6 @@ app.post('/', function (req, res) {
     res.sendStatus(200);
   }
 });
-
-function receivedMessage(event) {
-    var senderID = event.sender.id;
-    var recipientID = event.recipient.id;
-    var timeOfMessage = event.timestamp;
-    var message = event.message;
-
-    console.log("Received message for user %d and page %d at %d with message:", senderID, recipientID, timeOfMessage);
-    console.log(JSON.stringify(message));
-
-    var messageId = message.mid;
-
-    // You may get a text or attachment but not both
-    var messageText = message.text;
-    var messageAttachments = message.attachments;
-
-    if (messageText) {
-
-        // If we receive a text message, check to see if it matches any special
-        // keywords and send back the corresponding example. Otherwise, just echo
-        // the text we received.
-        switch (messageText) {
-            case 'image':
-                sendImageMessage(senderID);
-            break;
-
-            case 'button':
-                sendButtonMessage(senderID);
-            break;
-
-            case 'generic':
-                sendGenericMessage(senderID);
-            break;
-
-            case 'receipt':
-                sendReceiptMessage(senderID);
-            break;
-
-            default:
-                sendTextMessage(senderID, messageText);
-        }
-    } else if (messageAttachments) {
-            sendTextMessage(senderID, "Message with attachment received");
-    }
-}
-
-function receivedDeliveryConfirmation(messagingEvent) {
-
-}
-
-function sendTextMessage(recipientId, messageText) {
-    var messageData = {
-        recipient: {
-        id: recipientId
-        },
-        message: {
-        text: messageText
-        }
-    };
-    callSendAPI(messageData);
-}
-
-function callSendAPI(messageData) {
-    request({
-        uri: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: { access_token: config.acces_token },
-        method: 'POST',
-        json: messageData
-
-    }, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-        var recipientId = body.recipient_id;
-        var messageId = body.message_id;
-
-        console.log("Successfully sent generic message with id %s to recipient %s", messageId, recipientId);
-        } else {
-            console.error("Unable to send message.");
-            //console.error(response);
-            console.log('RESPONSE');
-            console.error(error);
-        }
-    });  
-}
 
 app.listen(config.port, function () {
     console.log('Example app listening on port 8888!');
