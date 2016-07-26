c0376606ac41824d79f3779906172f57dfdd69e5
Ayaz Badouraly <ayaz.badouraly@via.ecp.fr>
2016-07-26 13:05:28 +0200

    Updating config

--- machine_learning_test/predict.py
+++ machine_learning_test/predict.py
@@ -5,13 +5,16 @@ import json, re, sys
 
 if __name__ == '__main__':
 
+    input = sys.argv[1].lower()
     res = { "ok" : True }
 
-    if sys.argv[1].lower() in ['bonjour', 'salut']:
+    if input in ['bonjour', 'salut']:
         res["type"] = "greetings"
-    elif re.match( r"^je m'appelle (.*)", sys.argv[1].lower() ):
+
+    elif re.match( r"^je m'appelle (.*)", input ):
         res["type"] = "introduction"
         res["info"] = {}
+
     else:
         res["ok"] = False
 
