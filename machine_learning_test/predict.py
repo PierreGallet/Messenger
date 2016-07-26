ffe5cb3cd5bbcad2c5c3cebbfa8d637fd57f82b4
Ayaz Badouraly <ayaz.badouraly@via.ecp.fr>
2016-07-26 12:40:53 +0200

    Improving python test script

--- machine_learning_test/predict.py
+++ machine_learning_test/predict.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python2
 
 from __future__ import print_function
-import json, sys
+import json, re, sys
 
 if __name__ == '__main__':
 
-    res = {}
+    res = { "ok" : True }
+
     if sys.argv[1].lower() in ['bonjour', 'salut']:
-        res["ok"] = True
         res["type"] = "greetings"
+    elif re.match( r"^je m'appelle (.*)", sys.argv[1].lower() ):
+        res["type"] = "introduction"
+        res["info"] = {}
+    else:
+        res["ok"] = False
 
     print(json.dumps(res))
